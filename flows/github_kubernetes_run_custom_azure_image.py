70c217207a0ab8bd8f602a90025c3d84ee4c02a3
Anna Geller <anna.m.geller@gmail.com>
2021-10-22 19:36:00 +0200

    Create github_kubernetes_run_custom_azure_image.py

---
+++ flows/github_kubernetes_run_custom_azure_image.py
@@ -0,0 +1,32 @@
+from prefect import Flow, task
+from prefect.storage import GitHub
+from prefect.run_configs import KubernetesRun
+# the import below are only to demonstrate that custom modules were installed in the ECR image "community"
+from flow_utilities.db import get_df_from_sql_query
+
+
+FLOW_NAME = "github_kubernetes_run_custom_azure_image"
+STORAGE = GitHub(
+    repo="anna-geller/packaging-prefect-flows",
+    path=f"flows/{FLOW_NAME}.py",
+    access_token_secret="GITHUB_ACCESS_TOKEN",  # required with private repositories
+)
+
+
+@task(log_stdout=True)
+def hello_world():
+    text = f"hello from {FLOW_NAME}"
+    print(text)
+    return text
+
+
+with Flow(
+    FLOW_NAME,
+    storage=STORAGE,
+    run_config=KubernetesRun(
+        image="prefectdemos.azurecr.io/community/flows",
+        labels=["aks"],
+        image_pull_secrets=["aks"],  # see README
+    ),
+) as flow:
+    hw = hello_world()
