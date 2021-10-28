912a50a097428bef59f25d39dfa8c2880e7a8532
Anna Geller <anna.m.geller@gmail.com>
2021-10-28 22:21:50 +0200

    Create github_kubernetes_run_custom_gcr_image.py
    adding GCR K8s example

---
+++ flows/github_kubernetes_run_custom_gcr_image.py
@@ -0,0 +1,34 @@
+from prefect import Flow, task
+from prefect.storage import GitHub
+from prefect.run_configs import KubernetesRun
+from prefect.client.secrets import Secret
+# the import below are only to demonstrate that custom modules were installed in the ECR image "community"
+from flow_utilities.db import get_df_from_sql_query
+
+
+AWS_ACCOUNT_ID = Secret("AWS_ACCOUNT_ID").get()
+FLOW_NAME = "github_kubernetes_run_custom_gcr_image"
+STORAGE = GitHub(
+    repo="anna-geller/packaging-prefect-flows",
+    path=f"flows/{FLOW_NAME}.py",
+    access_token_secret="GITHUB_ACCESS_TOKEN",  # required with private repositories
+)
+
+
+@task(log_stdout=True)
+def hello_world():
+    text = f"hello from {FLOW_NAME}"
+    print(text)
+    return text
+
+
+with Flow(
+    FLOW_NAME,
+    storage=STORAGE,
+    run_config=KubernetesRun(
+        image="gcr.io/prefect-community/demos/community:latest",
+        labels=["k8s"],
+        image_pull_secrets=["gcr-secret"],
+    ),
+) as flow:
+    hw = hello_world()
